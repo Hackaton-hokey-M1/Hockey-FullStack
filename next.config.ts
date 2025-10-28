5e6edf598b4cc25c4b4825f4041f8cef71c8e069
A-Fevre <adrien.fevre.pro@gmail.com>
2025-10-28 20:38:19 +0100

    fix prisma

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+import withPrismaPlugin from '@prisma/nextjs-monorepo-workaround-plugin';
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+
+export default withPrismaPlugin(withNextIntl(nextConfig));
